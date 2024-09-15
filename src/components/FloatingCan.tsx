4e2ed08ed01aaffeb69933b92298ea1df994229e
shantanuvijay01 <138570333+shantanuvijay01@users.noreply.github.com>
2024-09-15 18:13:58 +0530

    Initial commit

---
+++ src/components/FloatingCan.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Float } from '@react-three/drei'
+import { SodaCan, SodaCanProps } from '@/components/SodaCan'
+import { forwardRef, ReactNode } from 'react';
+import { Group } from 'three';
+
+type FloatingCanProps = {
+    flavour?: SodaCanProps["flavour"];
+    floatSpeed?: number;
+    rotationIntensity?: number;
+    floatIntensity?: number;
+    floatingRange?: [number, number];
+    children?: ReactNode;
+};
+
+const FloatingCan = forwardRef<Group, FloatingCanProps>(({
+    flavour = "lemonLime",
+    floatSpeed = 1.5,
+    rotationIntensity = 1,
+    floatIntensity = 1,
+    floatingRange = [-0.1, 0.1],
+    children,
+    ...props
+}, ref ) => {
+  return (
+    <group ref={ref} {...props}>
+        <Float 
+            speed={floatSpeed} // Animation speed, defaults to 1
+            floatIntensity={floatIntensity} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+            floatingRange={floatingRange} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+            rotationIntensity={rotationIntensity} // XYZ rotation intensity, defaults to 1
+        >
+            <SodaCan flavour={flavour}/>
+        </Float>
+    </group>
+  )
+})
+
+FloatingCan.displayName = "FloatingCan";
+
+export default FloatingCan
